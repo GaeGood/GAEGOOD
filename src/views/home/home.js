cc7db29de0144f05dd85b1468149637e853614e6
Gyuri092 <ggr0927@gmail.com>
2022-11-03 02:03:12 +0900

    feature: home.js 작성

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -2,12 +2,105 @@
 // 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
 // 코드 예시를 남겨 두었습니다.
 
-import * as Api from "/api.js";
-import { randomId } from "/useful-functions.js";
+// import { json } from "express";
+// import nodemon from "nodemon";
+// import * as Api from "/api.js";
+// import { randomId } from "/useful-functions.js";
 
 // 요소(element), input 혹은 상수
-const landingDiv = document.querySelector("#landingDiv");
-const greetingDiv = document.querySelector("#greetingDiv");
+// const landingDiv = document.querySelector("#landingDiv");
+// const greetingDiv = document.querySelector("#greetingDiv");
+const fabricBtn = document.querySelector(".btn-fabrics");
+const stickerBtn = document.querySelector(".btn-stickers");
+const card = document.querySelector(".card");
+const cards = document.querySelector(".cards");
+
+// function fabricBtn() {
+//   if (card.classList.contains("sticker")) {
+//     card.classList.add("hidden");
+//   }
+// }
+
+// function stickerBtn() {
+//   if (card.classList.contains("sticker")) {
+//     card.classList.add("hidden");
+//   }
+// }
+
+const className = "";
+const productName = "";
+const productDesc = "";
+const price = "";
+
+// 템플릿리터럴 안의 내용은 백에서 json 파일로 가져올 때..!!!
+
+function cardTemplateInsert() {
+  const jsonProductData = [
+    {
+      Type: "fabric",
+      name: "담요",
+      Description: "얼음도 녹일 수 있는 담요",
+      price: "5000",
+    },
+    {
+      Type: "other",
+      name: "머그컵",
+      Description: "머그컵",
+      price: "5000",
+    },
+    {
+      Type: "fabric",
+      name: "담요",
+      Description: "얼음도 녹일 수 있는 담요",
+      price: "5000",
+    },
+    {
+      Type: "other",
+      name: "머그컵",
+      Description: "머그컵",
+      price: "5000",
+    },
+  ];
+
+  // let result = [];
+  // jsonProductData.forEach((item) => {
+  //   result.push(item);
+  // });
+
+  const resultArray = jsonProductData.map((item) => {
+    return `<div class="card ${item.Type}">
+    <img src="elice-rabbit.png" class="card-img-top" alt="..." />
+    <div class="card-body"></div>
+            <div class="card-text card-text-title">${item.name}</div>
+            <div class="card-text card-spec">
+              ${item.Description} 
+            </div>
+            <div class="card-text">${item.price}</div>
+          </div>
+        </div>`;
+  });
+  return resultArray;
+}
+const thing = cardTemplateInsert().join("");
+
+cards.innerHTML += thing;
+
+// cards.append(resultArray[i]);
+// let apple = [a,b,c]
+// apple.join("")
+// 만약, 제이슨 데이터가 배열이 아닌 경우?
+
+// 목표 : 배열인 제이슨 데이터를 한 요소 별로 불러와서 걔네 값을 뽑아내고
+// 그 뽑아낸걸 템플릿에 innerHTML로 넣어준다.
+
+// 1. card 템플릿 리터럴 하나 생성(붕어빵틀) -완료
+// 2. 틀에다가 그.. fabric, sticker, 등등의 클래스명 넣어주기
+// 3. 이 만들어진 내용을 .innerHTML 로 넣어주기.
+
+fabricBtn.addEventListener("click", () => {
+  alert("제발 되라");
+});
+// stickerBtn.addEventListener("click", stickerBtn);
 
 addAllElements();
 addAllEvents();
