4472dc159e4a7a3b76990ddf731750f4d0a3caaa
JongHoonSon <roka1684@gmail.com>
2022-11-03 18:14:28 +0900

    test: CSR, SSR 방식 차이

--- src/app.js
+++ src/app.js
@@ -6,24 +6,31 @@ import {
   globalRouter,
   orderRouter,
   productRouter,
+  pugRouter,
 } from "./routers";
 import { errorHandler } from "./middlewares";
 import { viewsRouter } from "./routers/views-router";
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views2");
+
+app.use("/public", express.static("public"));
+
 app.use(cors());
 
-app.use(viewsRouter);
+// app.use("/", viewsRouter);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/users", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/products", productRouter);
-app.use("/api/orders", orderRouter);
-app.use("/api/", globalRouter);
+app.use("/pug", pugRouter);
+// app.use("/api/users", userRouter);
+// app.use("/api/auth", authRouter);
+// app.use("/api/products", productRouter);
+// app.use("/api/orders", orderRouter);
+// app.use("/api/", globalRouter);
 
 app.use(errorHandler);
 
