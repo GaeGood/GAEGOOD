3cbcb8a4aef0e17722bf7a0e029ddbd16d6e1ea4
Siyea <siyea0405@gmail.com>
2022-11-03 20:49:48 +0900

    update: category 업데이트 및 card CSS 업데이트

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,6 +1,7 @@
 import { addCommas } from "/useful-functions.js";
 
 const cards = document.querySelector(".cards");
+const cardCategories = document.querySelectorAll(".nav-item");
 
 fetch("/api/products")
   .then((res) => {
@@ -9,20 +10,26 @@ fetch("/api/products")
   .then((productList) => {
     const createCard = (item) => {
       return `<div class="card ${item.category}">
-    <a href='/api/products/${item._id}'>
-    <img src="elice-rabbit.png" class="card-img-top" alt="..." />
-    <div class="card-body"></div>
-            <div class="card-text card-text-title">${item.name}</div>
-            <div class="card-text card-spec">
-              ${item.shortDesc} 
-            </div>
-            <div class="card-text">${addCommas(item.price)}</div>
-          </div>
-          </a>
-        </div>`;
+      <a href='/api/products/${item._id}'>
+        <img src="elice-rabbit.png" class="card-img-top" alt="${item.name}" />
+        <div class="card-body">
+        <div class="card-text card-text-title">${item.name}</div>
+        <div class="card-text card-spec">${item.shortDesc}</div>
+        <div class="card-text">${addCommas(item.price)}</div>
+        </div>
+      </a>
+    </div>`;
     };
     productList.forEach((product) => {
       const newCard = createCard(product);
       cards.innerHTML += newCard;
     });
   });
+
+cardCategories.forEach((cardCategory) =>
+  cardCategory.addEventListener("click", (e) => {
+    const eleValue = e.target
+    //eleValue.style.display = "block";
+    alert(`${eleValue}를 클릭햇다!`);
+  })
+);
