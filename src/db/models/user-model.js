0041a195237409ff958b2043befbf257ab610349
JongHoonSon <roka1684@gmail.com>
2022-11-04 02:47:39 +0900

    fix: User 스키마 모델명 수정

--- src/db/models/user-model.js
+++ src/db/models/user-model.js
@@ -1,7 +1,7 @@
 import { model } from "mongoose";
 import { UserSchema } from "../schemas/user-schema";
 
-const User = model("users", UserSchema);
+const User = model("User", UserSchema);
 
 export class UserModel {
   async findByEmail(email) {
