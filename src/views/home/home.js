cf0ffb9107fbe534a4e97b812946f9e986a10eda
minyoung <918liter@gmail.com>
2022-11-03 19:13:55 +0900

    update: product detail 1차

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -9,17 +9,16 @@ fetch("/api/products")
   .then((productList) => {
     const createCard = (item) => {
       return `<div class="card ${item.category}">
-    <a href='/api/products/${item._id}'>
-    <img src="elice-rabbit.png" class="card-img-top" alt="..." />
-    <div class="card-body"></div>
-            <div class="card-text card-text-title">${item.name}</div>
-            <div class="card-text card-spec">
-              ${item.shortDesc} 
-            </div>
-            <div class="card-text">${addCommas(item.price)}</div>
-          </div>
-          </a>
-        </div>`;
+      <a href='/products/detail/${item._id}'>
+        <img src="elice-rabbit.png" class="card-img-top" alt="..." />
+        <div class="card-body"></div>
+        <div class="card-text card-text-title">${item.name}</div>
+        <div class="card-text card-spec">
+          ${item.shortDesc}
+        </div>
+        <div class="card-text">${addCommas(item.price)}</div>
+      </a>
+    </div>`;
     };
     productList.forEach((product) => {
       const newCard = createCard(product);
