55892d635c17d448db83dec9d8438ad9a7109035
Gyuri092 <ggr0927@gmail.com>
2022-11-05 02:43:46 +0900

    feature: cart 상품 목록 렌더링 구현중

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -8,36 +8,33 @@ const deliveryFee = document.querySelector(".deliveryFee");
 const total__sum = document.querySelector(".total__sum");
 const order__button = document.querySelector(".order__button");
 
-const databaseName = "cartDB";
-const version = 1;
-const objectStore = "cartStorage";
-let productData = [];
-let productData2 = "";
-/* indexedDB 데이터 전체 가져오기 */
-productData2 = getAllIndexedDB(databaseName, version, objectStore, productData);
-console.log(productData2);
-/* 상품 상제정보 불러오기*/
-// const idObject = { id: id };
-// fetch(`/api/products/${id}`)
-//   .then((res) => res.json())
-//   .then((product) => {
-//     addproduct(product);
-//   })
-//   .catch((err) => alert(err.message));
+/* 데이터 렌더링 */
+getAllIndexedDB(databaseName, version, objectStore, function (dataList) {
+  const cartContainer = document.querySelector(".cart__list__top");
+  if (dataList.length !== 0) {
+    dataRender(dataList);
+  }
+});
 
-// // home에서 클릭한 제품의 상세 내용 html에 렌더링하는 함수
-// function addproduct(product) {
-//   productCategory.innerHTML = product.category;
-//   productName.innerHTML = product.name;
-//   productImg.src = product.smallImageURL;
-
-//   productDescription.innerHTML = product.shortDesc;
-//   productPrice.innerHTML = product.price;
-// }
-
-/* 해당 indexedDB에 존재하는 모든 데이터 조회하기 */
-function getAllIndexedDB(databaseName, version, objectStore, productData) {
-  let thing = "";
+function getAllIndexedDB(databaseName, version, objectStore, cb) {
+  if (window.indexedDB) {
+    const request = indexedDB.open(databaseName, version);
+    request.onerror = (event) => console.log(event.target.errorCode);
+    request.onsuccess = function () {
+      const db = request.result;
+      const transaction = db.transaction(objectStore, "readwrite");
+      const store = transaction.objectStore(objectStore);
+      store.getAll().onsuccess = function (response) {
+        cb(response.target.result);
+      };
+    };
+  } else {
+    alert("해당 브라우저에서는 indexedDB를 지원하지 않습니다.");
+  }
+}
+/* 해당 indexedDB에 존재하는 모든 key 조회하기 */
+function getAllKeysIndexedDB(databaseName, version, objectStore, cb) {
+  // getAllKeysIndexedDB 함수를 완성해주세요.
   if (window.indexedDB) {
     const request = indexedDB.open(databaseName, version);
     request.onerror = (event) => console.log(event.target.errorCode);
@@ -45,13 +42,37 @@ function getAllIndexedDB(databaseName, version, objectStore, productData) {
       const db = request.result;
       const transaction = db.transaction(objectStore, "readonly");
       const store = transaction.objectStore(objectStore);
-      store.getAll().onsuccess = function (response) {
-        productData = response.target.result;
-        thing = productData[0].id;
+      store.getAllKeys().onsuccess = function (response) {
+        cd(response.target.result);
       };
     };
-    return thing;
   } else {
     alert("해당 브라우저에서는 indexedDB를 지원하지 않습니다.");
   }
+}
+/* 데이터 렌더링 */
+function dataRender(dataList) {
+  for (let i = 0; i < dataList.length; i++) {
+    const databaseName = "cartDB";
+    const version = 1;
+    const objectStore = "cartStorage";
+
+    getAllKeysIndexedDB(databaseName, version, objectStore, function (keys) {
+      const cartProductId = keys[i]; // posts ObjectStore에 있는 Key를 id로 사용해보세요.
+      /* 상품 상제정보 불러오기*/
+      fetch(`/api/products/${id}`)
+        .then((res) => res.json())
+        .then((product) => {
+          addproduct(product);
+        })
+        .catch((err) => alert(err.message));
+    });
+  }
+}
+
+// home에서 클릭한 제품의 상세 내용 html에 렌더링하는 함수
+function addproduct(product) {
+  total__price.innerHTML = product.price;
+  cart__detail__name.innerHTML = product.name;
+  cart__detail__image.src = product.smallImageURL;
 }
