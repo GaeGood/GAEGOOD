d6797b1a0280496abb3019e48de80c835ae64ebe
jaehun <skybloom7777@gmail.com>
2022-11-08 02:38:06 +0900

    conflict: conflict merge

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,92 +1,9 @@
 import { addCommas } from "/useful-functions.js";
-import { verifyToken } from "/verify-token.js";
-
-// verifyToken : 브라우저가 갖고 있는 JWT토큰을 서버로부터 검증 받는 함수
-// 검증 성공 시 => { verifySucceed: true, loggedInUser } 을 반환
-// 검증 실패 시 => { verifySucceed: false } 을 반환
-const verifyResult = await verifyToken();
-console.log("verifyResult");
-console.log(verifyResult);
-
-const { loggedInUser } = verifyResult;
-console.log("-------------------- 토큰 검증 종료 -------------------------");
+import { main } from "/main.js";
+const { loggedInUser } = await main();
 
 const cards = document.querySelector(".cards");
-const categories = document.querySelectorAll(".nav-item");
-const navAddLogin = document.querySelector(".navbar-nav");
-
-const modalLogin = document.querySelector("#modalLogin");
-
-const loginFormSubmit = document.querySelector(".login__submit__btn");
-const email = document.querySelector("#email");
-const password = document.querySelector("#password");
-
-const addLi = document.createElement("li");
-addLi.className = "nav-item";
-if (document.cookie === "") {
-  addLi.className += " login__btn";
-  addLi.innerHTML += `<a class="nav-link active" data-bs-toggle="modal" data-bs-target="#modalLogin"
-      aria-current="page" href="#none">로그인</a>`;
-  navAddLogin.prepend(addLi);
-} else {
-  addLi.className += " logout__btn";
-  addLi.innerHTML += `<a class="nav-link active" data-bs-toggle="modal" data-bs-target="#modalLogin"
-      aria-current="page" href="#none">로그아웃</a>`;
-  navAddLogin.prepend(addLi);
-
-  // 회원가입버튼 삭제 -> 마이페이지 버튼 나타내기
-  const joinHtml = document.querySelector(".nav-item.join")
-  joinHtml.remove()
-
-function removeLogoutLi() {
-  const logoutLi = document.querySelector(".logout__btn");
-  navAddLogin.removeChild(logoutLi);
-}
-
-loginFormSubmit.addEventListener("click", (event) => {
-  event.preventDefault;
-  alert(
-    `입력한 이메일과 비밀번호입니다\nemail: ${email.value}\npassword: ${password.value}`
-  );
-  fetch("/api/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: email.value,
-      password: password.value,
-    }),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      const resultMassage = data.resMsg.msg;
-      if (data.resCode === "200") {
-        //모달창이 닫히는 기능
-        document.getElementsByTagName("body")[0].className = "";
-        document.getElementsByTagName("body")[0].style = "none";
-        document.querySelector("#modalLogin").style = "display: none";
-        document.querySelector(".modal-backdrop").remove();
-
-        //로그인을 했으니 로그인 버튼을 없애고 로그아웃으로 교체
-        const addLi = document.createElement("li");
-        document.querySelector(".login__btn").style = "display: none";
-        addLi.className = "nav-item";
-        addLi.className += " logout__btn";
-        addLi.innerHTML += `<a class="nav-link active" href="#none">로그아웃</a>`;
-        navAddLogin.prepend(addLi);
-
-        // 회원가입버튼 삭제 -> 마이페이지 버튼 나타내기
-        const joinHtml = document.querySelector(".nav-item.join")
-        joinHtml.remove()
-        
-        const mypageHtml = document.querySelector(".nav-item.mypage")
-        mypageHtml.style.display = "";
-
-      }
-      alert(resultMassage);
-    });
-});
+const categories = document.querySelectorAll(".nav-item.category");
 
 const createCard = (item) => {
   return `<div class="card ${item.category.name}">
