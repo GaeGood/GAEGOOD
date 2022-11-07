5bb9ba861f7b1ee8810e5c195798c6fbaa82ade0
jaehun <skybloom7777@gmail.com>
2022-11-08 01:12:18 +0900

    fix: product service,model error handler 적용

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -2,38 +2,73 @@ import { productModel } from "../db";
 
 class ProductService {
   async addProduct(productInfo) {
-    const createdNewProduct = await productModel.create(productInfo);
-    return createdNewProduct;
+    try {
+      const createdNewProduct = await productModel.create(productInfo);
+      return createdNewProduct;
+    } catch (err) {
+      const error = new Error("상품 추가에 실패하였습니다.");
+      error.statusCode = 400;
+      throw error;
+    }
   }
 
   async getProductById(pid) {
-    const product = await productModel.findById(pid);
-    return product;
+    try {
+      const product = await productModel.findById(pid);
+      return product;
+    } catch (err) {
+      const error = new Error("ID기반 상품을 찾는데에 실패하였습니다.");
+      error.statusCode = 400;
+      throw error;
+    }
   }
 
   async getProductList(pidArr) {
-    if (!pidArr) {
-      const productList = await productModel.findAll();
-      return productList;
-    } else {
-      const productList = await productModel.findByIds(pidArr);
-      return productList;
+    try {
+      if (!pidArr) {
+        const productList = await productModel.findAll();
+        return productList;
+      } else {
+        const productList = await productModel.findByIds(pidArr);
+        return productList;
+      }
+    } catch (err) {
+      const error = new Error("상품 리스트를 불러오지 못하였습니다.");
+      error.statusCode = 400;
+      throw error;
     }
   }
 
   async editProduct(pid, productInfo) {
-    const updatedProduct = await productModel.update(pid, productInfo);
-
-    return updatedProduct;
+    try {
+      const updatedProduct = await productModel.update(pid, productInfo);
+      return updatedProduct;
+    } catch (err) {
+      const error = new Error("상품 수정에 실패하였습니다.");
+      error.statusCode = 400;
+      throw error;
+    }
   }
 
   async removeProduct(pid) {
-    await productModel.delete(pid);
+    try {
+      await productModel.delete(pid);
+    } catch (err) {
+      const error = new Error("상품 삭제에 실패하였습니다.");
+      error.statusCode = 400;
+      throw error;
+    }
   }
 
   async searchProduct(searchBy) {
-    const productList = await productModel.search(searchBy);
-    return productList;
+    try {
+      const productList = await productModel.search(searchBy);
+      return productList;
+    } catch (err) {
+      const error = new Error("상품 검색에 에 실패하였습니다.");
+      error.statusCode = 400;
+      throw error;
+    }
   }
 }
 
