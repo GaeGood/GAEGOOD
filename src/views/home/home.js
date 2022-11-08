1a4a18de3906a49fc7b1cb98b91d7663367846de
JongHoonSon <roka1684@gmail.com>
2022-11-09 03:08:38 +0900

    feat: 홈 페이지에서 사용되는 카테고리를 동적으로 불러오도록 구현

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -2,8 +2,18 @@ import { addCommas } from "/useful-functions.js";
 import { main } from "/main.js";
 const { loggedInUser } = await main();
 
+const categoryWrap = document.getElementById("category__wrap");
+
+const createCategory = (category) => {
+  return `
+        <li class="nav-item category">
+          <button class="btn__${category.name}">
+            <a class="nav-link" href="#">${category.name}</a>
+          </button>
+        </li>`;
+};
+
 const cards = document.querySelector(".cards");
-const categories = document.querySelectorAll(".nav-item.category");
 
 const createCard = (item) => {
   return `<div class="card ${item.category.name}">
@@ -20,6 +30,26 @@ const createCard = (item) => {
     </div>
   </div>`;
 };
+
+fetch("/api/categories")
+  .then(async (res) => {
+    const json = await res.json();
+
+    if (res.ok) {
+      return json;
+    }
+
+    return new Promise.reject(json);
+  })
+  .then((categoryList) => {
+    categoryList.forEach((category) => {
+      const categoryDiv = createCategory(category);
+      categoryWrap.innerHTML += categoryDiv;
+    });
+  })
+  .catch((e) => {
+    alert(e);
+  });
 
 fetch("/api/products")
   .then(async (res) => {
@@ -39,10 +69,19 @@ fetch("/api/products")
     return productList;
   }) //카테고리를 누르는것에 따라서 카테고리별 상품 이미지 띄우기
   .then((productList) => {
-    categories.forEach((category) => {
+    const categoryList = document.querySelectorAll(".nav-item.category");
+    console.log("categoryList");
+    console.log(categoryList);
+    categoryList.forEach((category) => {
       category.addEventListener("click", (event) => {
         cards.textContent = "";
         productList.forEach((product) => {
+          console.log("product.category.name");
+          console.log(product.category.name);
+          console.log("event.target.text");
+          console.log(event.target.text);
+          console.log("event.target");
+          console.log(event.target);
           if (product.category.name === event.target.text) {
             const newCard = createCard(product);
             cards.innerHTML += newCard;
