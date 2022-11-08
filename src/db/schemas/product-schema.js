288031f9f70a002e85c44394430be87649705b5c
JongHoonSon <roka1684@gmail.com>
2022-11-08 17:42:01 +0900

    fix: product-schema category 보기 좋게 변경

--- src/db/schemas/product-schema.js
+++ src/db/schemas/product-schema.js
@@ -6,7 +6,11 @@ const ProductSchema = new Schema(
       type: String,
       required: true,
     },
-    category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
     shortDesc: {
       type: String,
       required: true,
