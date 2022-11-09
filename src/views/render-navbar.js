dc7993528f38f43f259db9f61cb294a65e6a42dd
minyoung <918liter@gmail.com>
2022-11-09 19:49:37 +0900

    update: import css 경로 수정, navbar class명 일부 수정

--- src/views/render-navbar.js
+++ src/views/render-navbar.js
@@ -58,7 +58,7 @@ function renderNavbar(loggedInUser) {
      <a class="navbar-brand" href="/">GAEGOOD</a>
      <ul class="navbar-nav">
        <li class="nav-item login">
-         <a class="nav-link active nav-item" data-bs-toggle="modal" data-bs-target="#modalLogin" aria-current="page">로그인</a>
+         <a class="nav-link active" data-bs-toggle="modal" data-bs-target="#modalLogin" aria-current="page">로그인</a>
        </li>
        <li class="nav-item join__btn">
         <a class="nav-link active" data-bs-toggle="modal" data-bs-target="#modalJoin" href="#none">회원가입</a>
