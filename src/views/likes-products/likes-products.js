6481a773d4c83e976062f0cfbb33168f5c7614a7
minyoung <918liter@gmail.com>
2022-11-11 17:09:29 +0900

    update: 찜목록 페이지 구현 오나료

--- src/views/likes-products/likes-products.js
+++ src/views/likes-products/likes-products.js
@@ -1,162 +1,103 @@
-import { main, addCommas } from "/public/js/main.js";
+import { main } from "/public/js/main.js";
 const { loggedInUser } = await main();
 
 if (!loggedInUser) {
   window.location.href = "/";
 }
 
-console.log(loggedInUser);
+let { likesProductList, name, _id } = loggedInUser;
 
-const { likesProductList, name } = loggedInUser;
+const likeNone = document.querySelector(".like__none");
+const likesProductListZone = document.querySelector(".like__product__list");
 
-console.log(likesProductList);
-
-const mainMypageHeader = document.querySelector(".main__mypage__header");
-
-mainMypageHeader.innerHTML = `안녕하세요, ${name}님!`;
-
-orderListDefultSort();
-
-function orderListDefultSort() {
-  orderListZone.innerHTML = "";
-  orderList.reverse().forEach(orderListMake);
-}
-
-function orderListMake(order) {
-  // 주문내역이 있으면 주문내역 없다는 안내멘트 지우기
-
-  orderNone.className = "order__none hidden";
-}
-
-/*
-const { orderList, name } = loggedInUser;
-
-const orderNone = document.querySelector(".order__none");
-const orderListZone = document.querySelector(".order__list");
 const mainMypageHeader = document.querySelector(".main__mypage__header");
 mainMypageHeader.innerHTML = `안녕하세요, ${name}님!`;
 
-// 페이지 로드시 무조건 최근주문순으로
-if (orderList.length) {
-  orderListDefultSort();
-}
+likesProductListMake();
 
-function orderListDefultSort() {
-  orderListZone.innerHTML = "";
-  orderListZone.innerHTML = `
-          <div class="table__title">
-            <div class="table-header">주문정보</div>
-            <div class="table__title asc__desc">
-              <a class="order__sort"><div>오래된주문순</div></a>
+console.log("likesProductList", likesProductList);
+
+function likesProductListMake() {
+  likesProductList.forEach((likesProduct) => {
+    // 주문내역이 있으면 주문내역 없다는 안내멘트 지우기
+    likeNone.className = "like__none hidden";
+
+    const productid = likesProduct._id;
+    const productCategory = likesProduct.category;
+    const productName = likesProduct.name;
+    const productImgUrl = likesProduct.smallImageURL;
+
+    likesProductListZone.innerHTML += `
+          <div class="card" style="width: 15rem">
+            <div class="product__image__zone">
+              <a href="/products/${productid}"><img
+                src="${productImgUrl}"
+                class="card-img-top product__image"
+                alt="찜상품사진"
+              /></a>
             </div>
-  `;
-  orderList.reverse().forEach(orderListMake);
-
-  // 오래된주문순 클릭 시 오래된주문순으로 정렬 이벤트
-  const orderSort = document.querySelector(".order__sort");
-  orderSort.addEventListener("click", orderListOldSort);
-}
-
-// 오래된주문순정렬
-function orderListOldSort() {
-  orderListZone.innerHTML = "";
-  orderListZone.innerHTML = `
-          <div class="table__title">
-            <div class="table-header">주문정보</div>
-            <div class="table__title asc__desc">
-              <a class="order__sort"><div>최근주문순</div></a>
-            </div>
-  `;
-
-  orderList.reverse().forEach(orderListMake);
-
-  // 최근주문순 클릭 시 최근주문순으로 정렬 이벤트
-  const orderSort = document.querySelector(".order__sort");
-  orderSort.addEventListener("click", orderListDefultSort);
-}
-
-function orderListMake(order) {
-  // 주문내역이 있으면 주문내역 없다는 안내멘트 지우기
-
-  orderNone.className = "order__none hidden";
-
-  const orderId = order._id;
-  const countList = order.countList;
-  const productIdList = order.productList;
-  const orderDay = order.createdAt.split("T")[0];
-  const shippingStatus = order.shippingStatus;
-
-  orderListZone.innerHTML += `<div class="card order__contents">
-        <a href="/orders/${orderId}">
-        <div class="card-header">${orderDay} 주문</div>
-        <div class="orderzone__${orderId}" style="display:flex;align-items: center;justify-content: space-between;">
-          <div class="order__${orderId}"></div>
-          </a>
-        </div>
-  `;
-
-  for (let i = 0; i < countList.length; i++) {
-    fetch(`/api/products/${productIdList[i]}`)
-      .then(async (res) => {
-        const json = await res.json();
-        if (res.ok) {
-          return json;
-        }
-        return Promise.reject(json);
-      })
-      .then((product) => {
-        const productName = product.name;
-        const productImg = product.smallImageURL;
-        const productPrice = product.price;
-
-        // 상품정보 삽입
-        const dateOrder = document.querySelector(`.order__${orderId}`);
-        dateOrder.innerHTML += `
-                <div class="card-body">
-                  <div class="product__picture">
-                    <img src=${productImg} class="product__image"/>
-                  </div>
-                  <div class="product__information">
+            <div class="card-body">
+              <div class="like__zone">
+                <a href="/products/${productid}" class="product__information">
                     <h5 class="card-title">${productName}</h5>
-                    <span class="card-text">${addCommas(productPrice)}원</span>
-                    <span class="card-text"> / </span>
-                    <span class="card-text">${countList[i]}개</span>
-                  </div>
+                    <p class="card-text">${productCategory}</p>
+                </a>
+                <div class="buttons__container">
+                    <div class="like__button">
+                        <i class="fa-regular fa-heart hidden"></i>
+                        <i class="fa-solid fa-heart" id="${productid}"></i>
+                    </div>
                 </div>
-       `;
-      })
-      .catch((err) => alert(err));
-  }
+              </div>
+            </div>
+          </div>
+    
+    `;
+    const likeButton = document.querySelector(".like__product__list");
 
-  // 배송상태와 주문상세버튼 날짜별로 1개씩 추가
-  const orderZone = document.querySelector(`.orderzone__${orderId}`);
-  orderZone.innerHTML += `
-        <div>
-        <div class="etc__zone">
-          <div class="shipping__status__${orderId}">${shippingStatus}</div>
-          <a type="button" class="btn btn-outline-secondary" href="/orders/${orderId}">
-            주문상세</a>
-        </div>
-        </div>
-  `;
+    function likeButtonClick(e) {
+      if (e.target.id === `${productid}`) {
+        const answer = confirm("찜 목록에서 삭제하시겠습니까?");
 
-  // 배송현황 취소완료시 글씨 색 red로 변경
-  if (shippingStatus === "취소완료") {
-    const shippingStatusMessage = document.querySelector(
-      `.shipping__status__${orderId}`
-    );
-    shippingStatusMessage.style.color = "red";
-  }
+        if (answer) {
+          likesProductList = likesProductList.filter(
+            (likesProduct) => likesProduct._id !== e.target.id
+          );
+
+          fetch(`/api/users/${_id}`, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              likesProductList: likesProductList,
+            }),
+          })
+            .then(async (res) => {
+              const json = await res.json();
+
+              if (res.ok) {
+                return json;
+              }
+
+              return Promise.reject(json);
+            })
+            .then((userInfoChange) => {
+              alert("찜 목록에서 삭제되었습니다.");
+              window.location.href = "/users/likes-products/";
+            })
+            .catch((err) => {
+              alert(
+                `에러가 발생했습니다. 관리자에게 문의하세요. \n에러내용: ${err}`
+              );
+            });
+        }
+      }
+    }
+
+    likeButton.addEventListener("click", likeButtonClick);
+  });
 }
-
-// // 최근주문순 클릭 시 최근주문순으로 정렬 이벤트
-// const orderDesc = document.querySelector(".order__desc");
-// orderDesc.addEventListener("click", orderListRecentSort);
-
-// // 오래된주문순 클릭 시 오래된주문순으로 정렬 이벤트
-// const orderAsc = document.querySelector(".order__asc");
-// orderAsc.addEventListener("click", orderListOldSort);
-*/
 
 // 회원탈퇴 기능
 const deleteUserBtn = document.querySelector(".user__delete");
